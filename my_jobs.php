18bc1f023666fac889e7a3988f180b0f5e262585
talhaumer61 <talhaumer197@gmail.com>
2024-08-22 10:26:57 +0500

    Added email notification for new job listing

--- my_jobs.php
+++ my_jobs.php
@@ -4,6 +4,7 @@
     include "include/functions/functions.php";
     include "include/functions/login_func.php";
     checkLogin();
+    $title="My Jobs";
     include_once "include/header.php";
     include "include/navbar.php";
     include "include/user/my_jobs.php";
